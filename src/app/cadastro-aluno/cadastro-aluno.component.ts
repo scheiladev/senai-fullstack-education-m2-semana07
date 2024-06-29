e4703eb2deee27e04160f99275832b0846b69d3b
Scheila Stihler <sstihler@gmail.com>
2024-06-29 13:52:21 -0300

    Resolução do exercício 4

--- src/app/cadastro-aluno/cadastro-aluno.component.ts
+++ src/app/cadastro-aluno/cadastro-aluno.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro-aluno',
   standalone: true,
-  imports: [RouterModule],
+  imports: [ReactiveFormsModule, CommonModule, RouterLink],
   templateUrl: './cadastro-aluno.component.html',
   styleUrl: './cadastro-aluno.component.scss',
 })
-export class CadastroAlunoComponent {}
+export class CadastroAlunoComponent implements OnInit {
+  formCadastro!: FormGroup;
+
+  ngOnInit(): void {
+    this.formCadastro = new FormGroup({
+      nomeCompleto: new FormControl('', Validators.required),
+      cpf: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      celular: new FormControl('', Validators.required),
+      curso: new FormControl('', Validators.required),
+    });
+  }
+
+  salvar() {
+    window.alert('Usuário salvo com sucesso!');
+  }
+}
